e2dba17c6e2282cff6e596909529eb7ffa287ffa
Artes2517 <bro2517@yandex.ru>
2017-10-28 22:09:06 +0300

    Add function function checkPasswords

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -1,8 +1,17 @@
-function checkValidEmail(email) {
+function checkValidEmail() {
+  var email = document.getElementsByClassName("registration__login")[0].value;
   var result = email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
   if (!result){
-    alert('Введите валидный e-mail.');
     return false;
+  }
+  return true;
+}
+
+function checkPassword() {
+  var pass = document.getElementsByClassName("registration__password")[0].value; 
+  var passConfirm = document.getElementsByClassName("registration__password-repeat")[0].value;
+  if ((pass.length < 6) || (pass != passConfirm)) {
+    return false;  
   }
   return true;
 }
