b0ccbfd9732b31210c10e5d44fcb76b1dc188e9e
artes2517 <bro2517@yandex.ru>
2018-02-06 20:02:27 +0300

    Create method getCoffeeMenu

--- lw6/src/js/CoffeMachine.js
+++ lw6/src/js/CoffeMachine.js
@@ -1,10 +1,11 @@
 class CoffeMachine {
   constructor() {
     this._currentBalance = 0;
-    this._coffeeMenu = [ { id: 1, name: "Американо", price: 10 },
-                        { id: 2, name: "Латте", price: 15 },
-                        { id: 3, name: "Каппучино", price: 20 }
-                      ];
+    this._coffeeMenu = [ 
+                         { id: 1, name: "Американо", price: 10 },
+                         { id: 2, name: "Латте", price: 15 },
+                         { id: 3, name: "Каппучино", price: 20 }
+                       ];
     this._currentCoffeeNumber;
   }
 
@@ -20,4 +21,12 @@ class CoffeMachine {
     return cash === 1 || cash === 2 || cash === 5 || 
            cash === 10 || cash === 50 || cash === 100;
   }
+
+  getCoffeeMenu() {
+    this.coffeeMenu.forEach(function(coffe, i) {
+      console.log(coffe.id + ' - ' + coffe.name + ' (' + 
+                  coffe.price + ' руб.)');
+    }, this);
+  }
+  
 }
