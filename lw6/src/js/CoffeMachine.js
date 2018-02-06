afb9f85fb71888acb0c305488c978505405d9b55
artes2517 <bro2517@yandex.ru>
2018-02-06 20:25:13 +0300

    Add methods validations

--- lw6/src/js/CoffeMachine.js
+++ lw6/src/js/CoffeMachine.js
@@ -1,32 +1,49 @@
 class CoffeMachine {
   constructor() {
     this._currentBalance = 0;
-    this._coffeeMenu = [ 
+    this._coffeMenu = [ 
                          { id: 1, name: "Американо", price: 10 },
                          { id: 2, name: "Латте", price: 15 },
                          { id: 3, name: "Каппучино", price: 20 }
                        ];
-    this._currentCoffeeNumber;
+    this._currentCoffeeNumber = null;
   }
 
   setCash(cash) {
-    if (this.checkCash(cash)) {
-      this.balance += cash;
+    if (this.checkValidationCash(cash)) {
+      this._currentBalance += cash;
       return true;
     }
     return false;
   }
 
-  checkCash(cash) {
+  checkValidationCash(cash) {
     return cash === 1 || cash === 2 || cash === 5 || 
            cash === 10 || cash === 50 || cash === 100;
   }
 
   getCoffeeMenu() {
-    this.coffeeMenu.forEach(function(coffe, i) {
+    this._coffeMenu.forEach(function(coffe, i) {
       console.log(coffe.id + ' - ' + coffe.name + ' (' + 
-                  coffe.price + ' руб.)');
+                  coffe.price + ' руб.);');
     }, this);
   }
   
+  chooseCoffee(coffeeNumber) {
+    if (this.checkValidationNumberCoffee(coffeeNumber) && 
+        this.checkEnoughMoney(coffeeNumber - 1)) {
+      this._currentCoffeeNumber = coffeeNumber;
+      return true;
+    }
+    return false;
+  }
+
+  checkEnoughMoney(coffeeNumber) {
+    return (this._currentBalance >= this._coffeeMenu[coffeeNumber].price);
+  }
+
+  checkValidationNumberCoffee(number) {
+    return number in [1, this._coffeeMenu.length];
+  }
+
 }
