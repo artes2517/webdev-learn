969462c17013793e237244ddf530f3c636920f12
artes2517 <bro2517@yandex.ru>
2018-02-07 22:36:09 +0300

    Add tests for method checkValidationNumberCoffee

--- lw7/tests/tests.js
+++ lw7/tests/tests.js
@@ -38,3 +38,37 @@ describe('Метод checkValidationCash', function() {
     makeTest(arrange.value, arrange.expected);
   });
 });
+
+describe('Метод checkValidationNumberCoffee', function() {
+  function makeTest(value, expected) {
+    it(`принимает ${value} и должен вернуть ${expected}`, function() {
+      let testCoffeMachine = new CoffeeMachine();
+      let actual = testCoffeMachine.checkValidationNumberCoffee(value);
+      assert.equal(expected, actual);
+    });
+  }
+  let arranges = [
+                  { value: -5, expected: false },
+                  { value: -10, expected: false },
+                  { value: -2, expected: false },
+                  { value: -1, expected: false },
+                  { value: -5000, expected: false },
+                  { value: 0, expected: false },
+                  { value: 1000, expected: false },
+                  { value: 500, expected: false },
+                  { value: 15, expected: false },
+                  { value: 3500, expected: false },
+                  { value: 11, expected: false },
+                  { value: 13, expected: false },
+                  { value: 51, expected: false },
+                  { value: 9, expected: false },
+                  { value: null, expected: false },
+                  { value: undefined, expected: false },
+                  { value: 1, expected: true },
+                  { value: 2, expected: true },
+                  { value: 3, expected: true }
+                ];
+  arranges.forEach(function(arrange, i) {
+    makeTest(arrange.value, arrange.expected);
+  });
+});
